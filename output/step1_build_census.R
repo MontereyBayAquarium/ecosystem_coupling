37bd3ea284eae6aac0925f85ce7b1ddd1b4714cf
Joshua Smith <joshsmith@nceas.ucsb.edu>
2024-01-24 16:06:24 -0800

    working

--- output/step1_build_census.R
+++ output/step1_build_census.R
@@ -44,30 +44,58 @@ census_build1 <- census_orig %>%
                   rename(atos_id = atos)%>%
                   #join atos geometry
                   left_join(atos_build1, by = "atos_id", relationship = "many-to-many") %>%
+                  select(-geometry, -trend5yr)%>%
                   #convert density estimates to counts
-                  mutate(num_indep = lin_dens*area_orig,
-                         num_pup = num_indep*pupratio
-                         )
+                  mutate(n_indep = lin_dens*area_km2,
+                         n_pup = n_indep*pupratio,
+                         #set fields
+                         atos_id = as.character(atos_id),
+                         area_m2 = as.character(area_m2)
+                         ) 
 
 #check trends to see if the numbers make sense
 
 summarized_data <- census_build1 %>%
   group_by(year) %>%
-  summarize(total_num_indep = sum(num_indep))
+  summarize(total_n_indep = sum(n_indep))
+
+base_theme <-  theme(axis.text=element_text(size=10, color = "black"),
+                     axis.title=element_text(size=10,color = "black"),
+                     plot.tag=element_text(size=9,color = "black"),
+                     plot.title=element_text(size=12,color = "black", face = "bold"),
+                     # Gridlines
+                     panel.grid.major = element_blank(), 
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(), 
+                     axis.line = element_line(colour = "black"),
+                     # Legend
+                     legend.key.size = unit(0.3, "cm"), 
+                     #legend.key = element_rect(fill = "white"), # Set it to transparent
+                     legend.spacing.y = unit(0.1, "cm"),  
+                     legend.text=element_text(size=8,color = "black"),
+                     legend.title=element_blank(),
+                     #legend.key.height = unit(0.1, "cm"),
+                     #legend.background = element_rect(fill=alpha('blue', 0)),
+                     #facets
+                     strip.text = element_text(size=10, face = "bold",color = "black", hjust=0),
+                     strip.background = element_blank())
 
 # Plotting the summarized data
-ggplot(summarized_data, aes(x = year, y = total_num_indep)) +
+g <- ggplot(summarized_data, aes(x = year, y = total_n_indep)) +
   geom_line() +
   geom_point() +
-  labs(title = "Total num_indep by Year",
+  labs(title = "",
        x = "Year",
-       y = "Total num_indep")
+       y = "No. independents")+
+  theme_bw() + base_theme
 
+#ggsave(g, file = file.path("/Users/jossmith/Downloads/indep_plot.png"), width = 6,
+ #      height = 5, units = "in")
 
 ################################################################################
 #Step 3 - export
 
-write.csv(census_build1, file = file.path(basedir, "processed/census/census_data_processed.csv"), row.names = FALSE)
+write.csv(census_build1, file = file.path(basedir, "processed/census/census_data_processed.csv"))
 
 
 
