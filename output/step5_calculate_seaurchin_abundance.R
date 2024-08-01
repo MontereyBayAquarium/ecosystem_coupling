3bea105099aa069e004162d7a385f912ecb6821e
Joshua Smith <joshsmith@nceas.ucsb.edu>
2024-08-01 11:07:47 -0700

    working

---
+++ output/step5_calculate_seaurchin_abundance.R
@@ -0,0 +1,17 @@
+#Sea urchin abundance data processing
+#Joshua G. Smith
+#August 1, 2024
+
+rm(list=ls())
+librarian::shelf(tidyverse, here)
+
+
+###subtidal monitoring data were accessed at
+###https://opc.dataone.org/view/doi%3A10.25494%2FP6%2FMLPA_kelpforest.7
+
+##require:
+
+#MLPA_kelpforest_swath.4.csv
+#MLPA_kelpforest_site_table.4.csv
+#MLPA_kelpforest_taxon_table.4.csv
+
